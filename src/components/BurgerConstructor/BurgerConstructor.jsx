bb8236d54590a2e1344fe075f69ccdb23f722edf
sivanov <sivanov@avancore.ru>
2022-03-01 16:52:44 +0300

    Добавлен конструктор бургеров

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { ConstructorElement, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Button, ConstructorElement, CurrencyIcon, DragIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './BurgerConstructor.module.css';
 import dataBurgers from '../../utils/burger.json';
 
@@ -10,33 +10,33 @@ export default function BurgerConstructor() {
     const bun = burgerComponents.find(el => el.type === 'bun');
     const outherComponents = burgerComponents.filter(el => el.type !== 'bun');
 
-    const blokedItem = ({type, id, name, price, image}) => 
-                
-                <div className={`ml-6 pr-4 ${styles.burger_item}`}>
-                    <ConstructorElement
-                        key={id}
-                        type={type}
-                        isLocked={true}
-                        text={name}
-                        price={price}
-                        thumbnail={image}
-                    />
-                </div>;
+    const blokedItem = ({ type, id, name, price, image }) =>
+
+        <div className={`ml-6 pr-4`}>
+            <ConstructorElement
+                key={id}
+                type={type}
+                isLocked={true}
+                text={name}
+                price={price}
+                thumbnail={image}
+            />
+        </div>;
 
     return (
 
         <section className={`${styles.components_container} page__section mt-25`}>
 
-            {bun && blokedItem({ ...bun, type: 'top'})}
+            {bun && blokedItem({ ...bun, type: 'top' })}
 
-            <ul className={`${styles.burger_items} mr-1 custom-scroll`}>
+            <ul className={`${styles.burger_items} mt-4 mb-4 mr-1 custom-scroll`}>
                 {
                     outherComponents
                         .map(el =>
-                            <li className={`${styles.burger_item} pr-2`} key={el._id}>
+                            <li className={`${styles.burger_item} pr-2 mb-4`} key={el._id}>
                                 <div className={styles.drag_icon}>
                                     <DragIcon type="primary" />
-                                </div>                                
+                                </div>
                                 <ConstructorElement
                                     text={el.name}
                                     price={el.price}
@@ -47,7 +47,18 @@ export default function BurgerConstructor() {
                 }
             </ul>
 
-            {bun && blokedItem({ ...bun, type: 'bottom'})}
+            {bun && blokedItem({ ...bun, type: 'bottom' })}
+
+            <div className={`${styles.order} mt-10 mr-4`}>
+                <p className="text text_type_digits-medium">610</p>
+                <div className={`ml-2 mr-10 ${styles.currency_icon}`}>
+                    <CurrencyIcon type="primary"/>
+                </div>
+                
+                <Button type="primary" size="large">
+                    Оформить заказ
+                </Button>
+            </div>
 
         </section>
     );
--- src/components/ListIngredients/ListIngredients.jsx
+++ src/components/ListIngredients/ListIngredients.jsx
@@ -4,21 +4,24 @@ import Card from "../Card/Card";
 import { useIngredients } from "../hooks/ingredient-hooks";
 import styles from './ListIngredients.module.css';
 
-const ListIngredients=forwardRef( ({name, type}, ref)=> {
+const ListIngredients = forwardRef(({ name, type }, ref) => {
 
-    const {ingredients} = useIngredients();
+    const { ingredients } = useIngredients();
 
     return (
         <article>
             <p ref={ref} className="mb-6 text text_type_main-medium">{name}</p>
-            <div className={`${styles.ingrediens_wrapper} pl-4 pr-4 mb-10`}>
+            <ul className={`${styles.ingrediens_wrapper} pl-4 pr-4 mb-10`}>
                 {ingredients
                     .filter(el => el.type === type)
                     .map(el =>
-                        <Card key={el._id} imgSrc={el.image} {...el} />
+                        <li key={el._id} className={`${styles.card_item} mr-6`}>
+                            <Card imgSrc={el.image} {...el} />
+                        </li>
+
                     )
                 }
-            </div>
+            </ul>
 
         </article>);
 });
